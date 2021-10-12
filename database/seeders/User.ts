ef75627d5a41fd831696ad2da2b5b8de73507a7c
deevanych <cashalotkzn@gmail.com>
2021-10-12 20:41:42 +0300

    Add main seeder

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -1,8 +1,10 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 
-import { UserFactory } from 'Database/factories/user'
+import { UserFactory } from 'Database/factories/User'
 
 export default class UserSeeder extends BaseSeeder {
+  public static developmentOnly = true
+
   public async run () {
     await UserFactory.createMany(100)
   }
