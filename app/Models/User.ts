db1e59b5a596f681f82aa490f57381835ebbc3f7
deevanych <cashalotkzn@gmail.com>
2021-10-12 19:59:00 +0300

    Change user model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,6 +1,8 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
+const defaultUserAvatar = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png'
+
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number
@@ -11,7 +13,9 @@ export default class User extends BaseModel {
   @column({ serializeAs: null })
   public email: string
 
-  @column()
+  @column({
+    serialize: (avatarUrl: string | null) => avatarUrl ?? defaultUserAvatar,
+  })
   public avatar_url: string
 
   @column({ serializeAs: null })
