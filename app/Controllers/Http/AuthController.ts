c3f1df9d1e5f29f32153732ba806001af247e1e2
deevanych <cashalotkzn@gmail.com>
2021-10-14 00:53:41 +0300

    Add auth method

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,7 +1,12 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import * as faker from 'faker'
+import { CREATED_USERS_COUNT } from 'Database/seeders/User'
 
 export default class AuthController {
-  public async login ({}: HttpContextContract): Promise<string> {
-    return 'we'
+  public async login ({ auth }): Promise<string> {
+    const userId = faker.datatype.number({ min: 1, max: CREATED_USERS_COUNT })
+    const user = User.findOrFail(userId)
+
+    return await auth.use('api').generate(user)
   }
 }
