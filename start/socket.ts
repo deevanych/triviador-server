76689f51001e60dee2f7ce35f789abf6c749ca2d
cashalotkzn <cashalotkzn@gmail.com>
2021-10-15 03:27:03 +0300

    Add emit server information when client connected/disconnected

--- start/socket.ts
+++ start/socket.ts
@@ -1,29 +1,18 @@
 import Ws from 'App/Services/Ws'
-import User from 'App/Models/User'
-import { Socket } from 'socket.io'
 Ws.boot()
 
-/**
- * Listen for incoming socket connections
- */
-
-class UserInfoSocket extends Socket {
-  public rating: number;
-  public userId: number;
+const getData = (engine) => {
+  return {
+    playersCount: engine.clientsCount,
+    lookingForGamePlayersCount: 0,
+  }
 }
 
-Ws.io.on('connection', (socket: UserInfoSocket) => {
-  socket.on('joinPlayersQueueRoom', () => {
-    User.find(Math.floor(Math.random() * (3 - 1) + 1)).then((data: User) => {
-      socket.rating = data.rating;
-      socket.userId = Math.floor(Math.random() * (3 - 1) + 1);
-      socket.join('playersQueueRoom');
-    });
+Ws.io
+  .on('connection', (socket) => {
+    Ws.io.emit('serverInfo', getData(Ws.io.engine))
 
-    const users = Ws.io.sockets.adapter.rooms.get('playersQueueRoom');
-    users?.forEach((socketId) => {
-      const user: UserInfoSocket = Ws.io.sockets.sockets.get(socketId) as UserInfoSocket;
-      console.log(user.rating);
-    });
-  });
-})
+    socket.on('disconnect', () => {
+      Ws.io.emit('serverInfo', getData(Ws.io.engine))
+    })
+  })
