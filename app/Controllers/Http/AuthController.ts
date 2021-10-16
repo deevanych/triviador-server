bc4d4ed8a5523a6d80d9ae6cd9eaff056dc34078
cashalotkzn <cashalotkzn@gmail.com>
2021-10-17 00:26:30 +0300

    Add get auth user method

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -3,6 +3,10 @@ import * as faker from 'faker'
 import { CREATED_USERS_COUNT } from 'Database/seeders/User'
 
 export default class AuthController {
+  public async getAuthUser({ auth }): Promise<number> {
+    return await auth.user
+  }
+
   public async login ({ auth }): Promise<string> {
     const userId = faker.datatype.number({ min: 1, max: CREATED_USERS_COUNT })
     const user = User.findOrFail(userId)
