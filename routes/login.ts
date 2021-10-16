db73617e51fcbb05b5c5406c7689385fbc4780ce
cashalotkzn <cashalotkzn@gmail.com>
2021-10-17 00:27:09 +0300

    Add get auth user method

--- routes/login.ts
+++ routes/login.ts
@@ -2,3 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('login', 'AuthController.login')
   .as('login')
+Route.get('auth', 'AuthController.getAuthUser')
+  .as('getAuthUser')
+  .middleware('auth')
