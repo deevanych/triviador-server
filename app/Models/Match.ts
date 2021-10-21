7fbab48cc139a2bde97862e39d428362c6cae375
cashalotkzn <cashalotkzn@gmail.com>
2021-10-21 23:01:02 +0300

    Add 'is_completed' column, add users relation

--- app/Models/Match.ts
+++ app/Models/Match.ts
@@ -1,13 +1,24 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import User from 'App/Models/User'
 
 export default class Match extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public isCompleted: boolean
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @manyToMany(() => User, {
+    pivotTable: 'user_matches',
+    pivotColumns: ['amount'],
+    serializeAs: null
+  })
+  public users: ManyToMany<typeof User>
 }
--- database/migrations/1634063126013_matches.ts
+++ database/migrations/1634063126013_matches.ts
@@ -6,6 +6,7 @@ export default class Matches extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.boolean('is_completed').defaultTo(0)
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
