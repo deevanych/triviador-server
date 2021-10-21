e880ad86af2174a4ed22f534d73eda1de8fe721b
deevanych <cashalotkzn@gmail.com>
2021-10-21 04:35:28 +0300

    Add stopFindGame method

--- start/socket.ts
+++ start/socket.ts
@@ -1,22 +1,30 @@
 import Ws from 'App/Services/Ws'
 Ws.boot()
 
-const getData = (engine) => {
+const findGameRoomTitle = 'findGameRoom'
+
+const getData = (io) => {
   return {
-    playersCount: engine.clientsCount,
-    lookingForGamePlayersCount: 0,
+    playersCount: io.engine.clientsCount,
+    lookingForGamePlayersCount: io.sockets.adapter.rooms.get(findGameRoomTitle)?.size ?? 0,
   }
 }
 
 Ws.io
   .on('connection', (socket) => {
-    Ws.io.emit('serverInfo', getData(Ws.io.engine))
+    Ws.io.emit('serverInfo', getData(Ws.io))
 
     socket.on('disconnect', () => {
-      Ws.io.emit('serverInfo', getData(Ws.io.engine))
+      Ws.io.emit('serverInfo', getData(Ws.io))
     })
 
     socket.on('findGame', () => {
-      socket.join('findGameRoom')
+      socket.join(findGameRoomTitle)
+      Ws.io.emit('serverInfo', getData(Ws.io))
+    })
+
+    socket.on('stopFindGame', () => {
+      socket.leave(findGameRoomTitle)
+      Ws.io.emit('serverInfo', getData(Ws.io))
     })
   })
