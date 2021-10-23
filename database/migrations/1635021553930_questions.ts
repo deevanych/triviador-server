a59031144b579cacc9cdb493a3121734657b7b47
cashalotkzn <cashalotkzn@gmail.com>
2021-10-24 00:03:53 +0300

    Add question model

---
+++ database/migrations/1635021553930_questions.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import { EXTENDED_TYPE } from 'App/Models/Question'
+
+export default class Questions extends BaseSchema {
+  protected tableName = 'questions'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('text').comment('Question text')
+      table.string('question_type').defaultTo(EXTENDED_TYPE)
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
