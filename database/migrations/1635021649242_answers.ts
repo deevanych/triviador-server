b36cde215576bd9868ad4510ae84f07f1765ed08
cashalotkzn <cashalotkzn@gmail.com>
2021-10-24 00:04:07 +0300

    Add answer model

---
+++ database/migrations/1635021649242_answers.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Answers extends BaseSchema {
+  protected tableName = 'answers'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('text').comment('Answer text')
+      table.boolean('is_right').defaultTo(false).comment('Is variant is right')
+      table
+        .integer('question_id')
+        .unsigned()
+        .references('question.id')
+        .onDelete('CASCADE')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
