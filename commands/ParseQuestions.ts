13adef2c4b2d63f0ad7fd116f404aa7e0a489ef3
cashalotkzn <cashalotkzn@gmail.com>
2021-10-24 03:35:09 +0300

    Add questions parser command

--- commands/ParseQuestions.ts
+++ commands/ParseQuestions.ts
@@ -1,19 +1,12 @@
 import { BaseCommand } from '@adonisjs/core/build/standalone'
-import axios from 'axios'
-import { parse } from 'node-html-parser'
-import Question from 'App/Models/Question'
-import Answer from 'App/Models/Answer'
-
-const SITE_URL = 'baza-otvetov.ru'
-const SITE_PATH = '/quiz/ask'
-const SITE_ANSWER_CHECK = '/quiz/check'
+import { QuestionsParserService } from 'App/Services/QuestionsParserService'
 
 export default class ParseQuestions extends BaseCommand {
 
   /**
    * Command name is used to run the command
    */
-  public static commandName = 'parse:question'
+  public static commandName = 'parse:questions'
 
   /**
    * Command description is displayed in the "help" output
@@ -35,50 +28,21 @@ export default class ParseQuestions extends BaseCommand {
   }
 
   public async run () {
-    try {
-      const questionHtml = await axios.get(`https://${SITE_URL}${SITE_PATH}`, {
-        headers: {
-          'x-requested-with': 'XMLHttpRequest',
-        },
-      })
+    let recurringQuestionsInRowCount = 0
+    let i = 0
 
-      const html = parse(questionHtml.data as string)
-      const question = html.querySelector('.q_id')
-      const questionText = question?.text
-      const questionId = question?.getAttribute('id')
-      const answers = html.querySelectorAll('h4')
-      const form = new URLSearchParams({
-        q_id: questionId as string,
-      })
-      const questionInstance = await Question.create(
-        {
-          text: questionText,
-        },
-      )
-      // console.log(questionInstance)
-      const answerHtml = await axios.post(
-        `https://${SITE_URL}${SITE_ANSWER_CHECK}`,
-        form,
-        {
-          headers: {
-            'x-requested-with': 'XMLHttpRequest',
-          },
-        },
-      )
-      const html2 = parse(answerHtml.data as string)
-      const rightAnswerBlock = html2.querySelector('h3 > span')?.text
-      const rightAnswer = rightAnswerBlock?.split('Правильный ответ: ')[1]
-
-      answers.forEach((answer) => {
-        const isRight = answer.text === rightAnswer
-        Answer.create({
-          text: answer.text,
-          isRight,
-          questionId: questionInstance.id,
-        })
-      })
-    } catch (e) {
-      console.log(e)
+    while (recurringQuestionsInRowCount < 1000) {
+      try {
+        await QuestionsParserService.run()
+        i++
+        recurringQuestionsInRowCount = 0
+        console.log(`Вопросов пёрнуто: ${i}`)
+      } catch (e) {
+        recurringQuestionsInRowCount++
+        console.log(`Количество повторяющихся вопросов подряд: ${recurringQuestionsInRowCount}`)
+      }
     }
+
+    process.exit(1)
   }
 }
