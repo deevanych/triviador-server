e776bb7cc5292446d7766032708ebfa5f93c71e0
deevanych <cashalotkzn@gmail.com>
2021-10-25 16:03:37 +0300

    Optimize match creation

--- app/Services/MatchService.ts
+++ app/Services/MatchService.ts
@@ -3,30 +3,29 @@ import { SocketExtended } from '../../start/socket'
 import Match from 'App/Models/Match'
 
 export const findGameRoomTitle = 'findGameRoom'
-const ratingMatchPlayersCount = 2
+const ratingMatchPlayersCount = 1
 
 export class MatchService {
-  public static matchmakingSelection = () => {
-    // @ts-ignore
-    Ws.io.in(findGameRoomTitle).fetchSockets().then((sockets: SocketExtended[]): void => {
-      const sortedSockets = sockets.sort((a: SocketExtended, b: SocketExtended) => {
-        const ratingA = a.user.rating ?? 0
-        const ratingB = b.user.rating ?? 0
+  static matchmakingSelection = async () => {
+    const sockets = await Ws.io.in(findGameRoomTitle).fetchSockets() as unknown as SocketExtended[]
+    const sortedSockets = sockets.sort((a: SocketExtended, b: SocketExtended) => {
+      const ratingA = a.user.rating ?? 0
+      const ratingB = b.user.rating ?? 0
 
-        return ratingA - ratingB
-      })
+      return ratingA - ratingB
+    })
 
-      if (sortedSockets.length >= ratingMatchPlayersCount) {
-        const readyUsers = sortedSockets.splice(0, ratingMatchPlayersCount)
-        new Match().save().then((match) => {
-          readyUsers.forEach((socket) => {
-            socket.leave(findGameRoomTitle)
-            match.related('users').attach([socket.user.id])
-            socket.join(match.getRoom)
-            socket.emit('startGame')
-          })
-        })
+    if (sortedSockets.length >= ratingMatchPlayersCount) {
+      const readyUsers = sortedSockets.splice(0, ratingMatchPlayersCount)
+      const match = await new Match().save()
+      const readyUsersIds = readyUsers.map((socket) => socket.user.id)
+      await match.related('users').attach(readyUsersIds)
+
+      for (let socket of readyUsers) {
+        socket.leave(findGameRoomTitle)
+        socket.join(match.getRoom)
+        socket.emit('startGame')
       }
-    });
+    }
   }
 }
