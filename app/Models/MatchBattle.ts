f8a656eb0bb258219390751b64b3cffb9ff2b930
cashalotkzn <cashalotkzn@gmail.com>
2021-11-23 20:52:19 +0300

    Add users relation to match battle

--- app/Models/MatchBattle.ts
+++ app/Models/MatchBattle.ts
@@ -1,9 +1,22 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import User from 'App/Models/User'
 
 export default class MatchBattle extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public question_id: number
+
+  @column()
+  public match_id: number
+
+  @manyToMany(() => User, {
+    pivotTable: 'match_battle_users',
+    pivotColumns: ['is_initiator']
+  })
+  public users: ManyToMany<typeof User>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
